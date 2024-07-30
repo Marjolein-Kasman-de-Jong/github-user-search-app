168e418372b3079168554ad9cae1d234eaa01319
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-07-30 12:22:00 +0200

    Finish page header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import useLocalStorage from 'use-local-storage';
 
+// Components
+import ThemeButton from './components/theme-button/ThemeButton';
+
 // Helpers
 import getData from './helpers/getData';
 
@@ -14,7 +17,7 @@ function App() {
 
   const switchTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light';
-    setTheme(newTheme); 
+    setTheme(newTheme);
   }
 
   const handleInputChange = (e) => {
@@ -34,128 +37,128 @@ function App() {
 
   return (
     <div className='page-wrapper' data-theme={theme}>
-      <header className='page-header'>
-        <h1 className='page-title heading-1'>
-          devfinder
-        </h1>
-        <button className='theme-button' onClick={switchTheme}>
-          {theme}
-        </button>
-      </header>
-      <main>
-        <form className='search-bar'>
-          <input className='paragraph-1' type='text' value={userToFind} onChange={handleInputChange} placeholder='Search GitHub username...' />
-          <button className='search-button' onClick={(e) => handleClick(e)}>
-            Search
-          </button>
-        </form>
-        <article className='card'>
-          <header className='card-header'>
-            <div className='avatar'>
-              <img src={data?.avatar} alt={data?.username} />
-            </div>
-            <div>
-              <hgroup>
-                <h2 className='username heading-2'>
-                  {data?.username}
-                </h2>
-                <p className='handle'>
-                  <a href={data?.profile} className='link-1'>
-                    {`@${data?.handle}`}
-                  </a>
+      <div className='content-wrapper'>
+        <header className='page-header'>
+          <h1 className='page-title heading-1'>
+            devfinder
+          </h1>
+          <ThemeButton theme={theme} onClick={switchTheme} />
+        </header>
+        <main>
+          <form className='search-bar'>
+            <input className='paragraph-1' type='text' value={userToFind} onChange={handleInputChange} placeholder='Search GitHub username...' />
+            <button className='search-button' onClick={(e) => handleClick(e)}>
+              Search
+            </button>
+          </form>
+          <article className='card'>
+            <header className='card-header'>
+              <div className='avatar'>
+                <img src={data?.avatar} alt={data?.username} />
+              </div>
+              <div>
+                <hgroup>
+                  <h2 className='username heading-2'>
+                    {data?.username}
+                  </h2>
+                  <p className='handle'>
+                    <a href={data?.profile} className='link-1'>
+                      {`@${data?.handle}`}
+                    </a>
+                  </p>
+                </hgroup>
+                <p className='joined paragraph-2'>
+                  {data?.joined}
                 </p>
-              </hgroup>
-              <p className='joined paragraph-2'>
-                {data?.joined}
-              </p>
-            </div>
-          </header>
-          <p className='bio paragraph-3'>
-            {data?.bio}
-          </p>
-          <div className='stats-container'>
-            <div className='stat'>
-              <h3 className='stat-title heading-3'>
-                Repos
-              </h3>
-              <p className='stat-number paragraph-4'>
-                {data?.repos}
-              </p>
-            </div>
-            <div className='stat'>
-              <h3 className='stat-title heading-3'>
-                Followers
-              </h3>
-              <p className='stat-number paragraph-4'>
-                {data?.followers}
-              </p>
-            </div>
-            <div className='stat'>
-              <h3 className='stat-title heading-3'>
-                Following
-              </h3>
-              <p className='stat-number paragraph-4'>
-                {data?.following}
-              </p>
-            </div>
-          </div>
-          <footer className='card-footer'>
-            <div className='url'>
-              <div className='icon paragraph-5'>
-                ic
               </div>
-              <div>
-                {data?.location ? <p className='paragraph-5'>{data.location}</p> : <p className='paragraph-5'>Not available</p>}
+            </header>
+            <p className='bio paragraph-3'>
+              {data?.bio}
+            </p>
+            <div className='stats-container'>
+              <div className='stat'>
+                <h3 className='stat-title heading-3'>
+                  Repos
+                </h3>
+                <p className='stat-number paragraph-4'>
+                  {data?.repos}
+                </p>
+              </div>
+              <div className='stat'>
+                <h3 className='stat-title heading-3'>
+                  Followers
+                </h3>
+                <p className='stat-number paragraph-4'>
+                  {data?.followers}
+                </p>
+              </div>
+              <div className='stat'>
+                <h3 className='stat-title heading-3'>
+                  Following
+                </h3>
+                <p className='stat-number paragraph-4'>
+                  {data?.following}
+                </p>
               </div>
             </div>
-            <div className='url'>
-              <div className='icon paragraph-5'>
-                ic
+            <footer className='card-footer'>
+              <div className='url'>
+                <div className='icon paragraph-5'>
+                  ic
+                </div>
+                <div>
+                  {data?.location ? <p className='paragraph-5'>{data.location}</p> : <p className='paragraph-5'>Not available</p>}
+                </div>
               </div>
-              <div>
-                {
-                  data?.blog ?
-                    <a href={data.blog} className='link-2'>
-                      {data.blog}
-                    </a>
-                    :
-                    <p className='paragraph-5'>Not available</p>
-                }
+              <div className='url'>
+                <div className='icon paragraph-5'>
+                  ic
+                </div>
+                <div>
+                  {
+                    data?.blog ?
+                      <a href={data.blog} className='link-2'>
+                        {data.blog}
+                      </a>
+                      :
+                      <p className='paragraph-5'>Not available</p>
+                  }
+                </div>
               </div>
-            </div>
-            <div className='url'>
-              <div className='icon paragraph-5'>
-                ic
+              <div className='url'>
+                <div className='icon paragraph-5'>
+                  ic
+                </div>
+                <div>
+                  {
+                    data?.twitter ?
+                      <a href={data.twitter} className='link-2'>
+                        {data.twitter}
+                      </a>
+                      :
+                      <p className='paragraph-5'>Not available</p>
+                  }
+                </div>
               </div>
-              <div>
-              {
-                  data?.twitter ?
-                    <a href={data.twitter} className='link-2'>
-                      {data.twitter}
-                    </a>
-                    :
-                    <p className='paragraph-5'>Not available</p>
-                }
+              <div className='url'>
+                <div className='icon paragraph-5'>
+                  ic
+                </div>
+                <div>
+                  {
+                    data?.company ?
+                      <a href={data.company} className='link-2'>
+                        {data.company}
+                      </a>
+                      :
+                      <p className='paragraph-5'>Not available</p>
+                  }
+                </div>
               </div>
-            </div>
-            <div className='url'>
-              <div className='icon paragraph-5'>
-                ic
-              </div>
-              <div>
-              {
-                  data?.company ?
-                    <a href={data.company} className='link-2'>
-                      {data.company}
-                    </a>
-                    :
-                    <p className='paragraph-5'>Not available</p>
-                }
-              </div>
-            </div>
-          </footer>
-        </article>
-      </main>
+            </footer>
+          </article>
+        </main>
+      </div>
     </div>
   )
 }
