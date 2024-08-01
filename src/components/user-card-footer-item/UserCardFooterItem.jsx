c8df96b07c3f70266e05c1374b4925af9a972949
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-08-01 13:32:58 +0200

    Move footer items to component

---
+++ src/components/user-card-footer-item/UserCardFooterItem.jsx
@@ -0,0 +1,44 @@
+// Styles
+import './user-card-footer-item.css';
+
+export default function UserCardFooterItem({ data, item }) {
+    console.log(data)
+    return (
+        <div className='user-card-footer-item'>
+            <div className={`icon paragraph-5 ${!data[item] && 'not-available'}`}>
+                <img
+                    src={`./public/images/icon-${item}.svg`}
+                    alt={item}
+                />
+            </div>
+            <div>
+                {
+                    data[item] ?
+                        item === 'location' || item === 'company' ?
+                            <p className={`paragraph-5 ${!data[item] && 'not-available'}`}>
+                                {data[item]}
+                            </p>
+                            :
+                            item === 'twitter' ?
+                                <a
+                                    href={`http://www.twitter.com/${data[item]}`}
+                                    className={`paragraph-5 link-2 ${!data[item] && 'not-available'}`}
+                                >
+                                    {data[item]}
+                                </a>
+                                :
+                                <a
+                                    href={data[item]}
+                                    className={`paragraph-5 link-2 ${!data[item] && 'not-available'}`}
+                                >
+                                    {data[item]}
+                                </a>
+                        :
+                        < p className={`paragraph-5 ${!data[item] && 'not-available'}`}>
+                            Not Available
+                        </p>
+                }
+            </div>
+        </div >
+    )
+}
--- src/components/user-card-footer/UserCardFooter.jsx
+++ src/components/user-card-footer/UserCardFooter.jsx
@@ -1,62 +1,19 @@
+// Components
+import UserCardFooterItem from '../user-card-footer-item/UserCardFooterItem';
+
 // Styles
 import './user-card-footer.css';
 
 export default function UserCardFooter({ data }) {
+    const items = ['location', 'blog', 'twitter', 'company'];
+
     return (
         <footer className='card-footer'>
-            <div className='url'>
-                <div className='icon paragraph-5'>
-                    ic
-                </div>
-                <div>
-                    {data?.location ? <p className='paragraph-5'>{data.location}</p> : <p className='paragraph-5'>Not available</p>}
-                </div>
-            </div>
-            <div className='url'>
-                <div className='icon paragraph-5'>
-                    ic
-                </div>
-                <div>
-                    {
-                        data?.blog ?
-                            <a href={data.blog} className='link-2'>
-                                {data.blog}
-                            </a>
-                            :
-                            <p className='paragraph-5'>Not available</p>
-                    }
-                </div>
-            </div>
-            <div className='url'>
-                <div className='icon paragraph-5'>
-                    ic
-                </div>
-                <div>
-                    {
-                        data?.twitter ?
-                            <a href={data.twitter} className='link-2'>
-                                {data.twitter}
-                            </a>
-                            :
-                            <p className='paragraph-5'>Not available</p>
-                    }
-                </div>
-            </div>
-            <div className='url'>
-                <div className='icon paragraph-5'>
-                    ic
-                </div>
-                <div>
-                    {
-                        data?.company ?
-                            <a href={data.company} className='link-2'>
-                                {data.company}
-                            </a>
-                            :
-                            <p className='paragraph-5'>Not available</p>
-                    }
-                </div>
-            </div>
+            {
+                items.map((item) => {
+                    return <UserCardFooterItem key={item} data={data} item={item} />
+                })
+            }
         </footer>
     )
 }
